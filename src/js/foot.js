47f6112adcd9dc1a5274a9f9e3d16df9dd9b8d99
zhengwen <930151250@qq.com>
2020-06-05 15:57:02 +0800

    优化代码的文件的放置并更新了一点详情页

---
+++ src/js/foot.js
@@ -0,0 +1,3 @@
+! function($) {
+    $('.commonFooter').load('http://10.31.162.40/dangdang/src/foot.html');
+}(jQuery);
---
+++ src/js/head.js
@@ -0,0 +1,3 @@
+! function($) {
+    $('.commonHeader').load('http://10.31.162.40/dangdang/src/head.html');
+}(jQuery);
